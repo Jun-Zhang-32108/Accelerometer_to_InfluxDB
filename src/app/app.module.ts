6d01fb0f30e6d3b677ee1491b8ef771958737fed
Naglis Bukauskas <naglisvbukauskas@gmail.com>
2020-02-18 22:08:32 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    DeviceMotion,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DebugElement, SystemJsNgModuleLoader } from '@angular/core';
+import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from '@ionic-native/device-motion/ngx';
+import { IonTextarea } from '@ionic/angular';
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  x: string;
+  y: string;
+  z: string;
+  output: string;
+  id: any;
+  textbox: IonTextarea;
+
+  constructor(public deviceMotion: DeviceMotion) {
+    this.x = "-";
+    this.y = "-";
+    this.z = "-";
+    this.output = "-";
+  }
+
+  startListening() {
+    //window.alert("Started Listening");
+    console.log("-----------BEGIN LISTENING-----------");
+
+    var option: DeviceMotionAccelerometerOptions =
+    {
+      //This is the polling frequency. It is measured in ms.
+      frequency: 50
+    };
+
+
+
+    this.id = this.deviceMotion.watchAcceleration(option).subscribe((acceleration: DeviceMotionAccelerationData) => {
+      this.x = "" + acceleration.x.toFixed(4);
+      this.y = "" + acceleration.y.toFixed(4);
+      this.z = "" + acceleration.z.toFixed(4);
+      this.output = " X: " + acceleration.x.toFixed(4) + " Y: " + acceleration.y.toFixed(4) + " Z: " + acceleration.z.toFixed(4);
+      console.log("Time Stamp: " + acceleration.timestamp + "" + this.output);
+    }
+    );
+  }
+
+  stopListening() {
+    //window.alert("Stopped Listening");
+    this.id.unsubscribe();
+    console.log("-----------STOP LISTENING-----------");
+
+  }
+
+  resetCurrentData() {
+    //window.alert("Data Reset");
+    this.id.unsubscribe();
+    this.x = "0";
+    this.y = "0";
+    this.z = "0";
+    this.output = "-";
+  }
 
 }
